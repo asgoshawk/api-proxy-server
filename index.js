d1a58ef02a67f093ff19d9224bbaec356b8222ac
asgoshawk <x5553434@gmail.com>
2022-07-26 16:44:09 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Enable cors
+app.use(cors())
+
+app.listen(PORT, ()=> console.log(`Server is runing on port ${PORT}`))
